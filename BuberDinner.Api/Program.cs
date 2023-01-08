650e5aebe2ec2be66295160449cb969fa52ffa48
Ugur Timurcin <utimurcin@outlook.com>
2023-01-08 17:05:57 +0300

    specified dependencies

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,3 +1,6 @@
+using BuberDinner.Application;
+using BuberDinner.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication()
+                .AddInfrastructure();
 
 var app = builder.Build();
 
