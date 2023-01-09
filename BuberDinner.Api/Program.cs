7be834ddb31a64ad73795e5093e7db841e9ab41c
Ugur Timurcin <utimurcin@outlook.com>
2023-01-09 21:54:53 +0300

    added dependencies

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplication()
-                .AddInfrastructure();
+                .AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
