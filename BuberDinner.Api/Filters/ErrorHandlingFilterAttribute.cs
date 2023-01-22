142627e5d0f2ef211f0655a2a8d324739cc1cf3c
Ugur Timurcin <utimurcin@outlook.com>
2023-01-22 22:25:18 +0300

    added error handling filter attribute

---
+++ BuberDinner.Api/Filters/ErrorHandlingFilterAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BuberDinner.Api.Filters;
+
+public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
+{
+    public override void OnException(ExceptionContext context)
+    {
+        var exception = context.Exception;
+
+        ProblemDetails problemDetails = new()
+        {
+            Title = "An error occured while processing your request.",
+            Status = StatusCodes.Status500InternalServerError,
+        };
+
+
+        context.Result = new ObjectResult(problemDetails)
+        {
+            StatusCode = StatusCodes.Status500InternalServerError
+        };
+
+        context.ExceptionHandled = true;
+    }
+}
