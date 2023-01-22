f62bf72397568fb72f0b84432685ad5404f5b8e3
Ugur Timurcin <utimurcin@outlook.com>
2023-01-22 22:25:55 +0300

    specified dependencies

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,11 +1,22 @@
+using BuberDinner.Api.Errors;
+using BuberDinner.Api.Filters;
+using BuberDinner.Api.Middleware;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//builder.Services.AddControllers(options =>
+//{
+//    options.Filters.Add<ErrorHandlingFilterAttribute>();
+//});
 builder.Services.AddControllers();
+
+builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,6 +32,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+//app.UseMiddleware<ErrorHandlingMiddleWare>();
+
+app.UseExceptionHandler("/error");
 
 app.UseHttpsRedirection();
 
