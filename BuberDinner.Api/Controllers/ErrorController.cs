87838d94ced59ad94551e7ff3dd449af6d17429d
Ugur Timurcin <utimurcin@outlook.com>
2023-01-22 22:24:14 +0300

    added error controller

---
+++ BuberDinner.Api/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BuberDinner.Api.Controllers;
+
+[ApiController]
+[ApiExplorerSettings(IgnoreApi = true)]
+public class ErrorController : ControllerBase
+{
+    [Route("/error")]
+    public IActionResult Error()
+    {
+        Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+        return Problem();
+    }
+}
